e7feffa2aeae634185324dd28bc99caed899110b
PhamPhuc1301 <phamphuc224@gmail.com>
2023-11-24 22:55:53 +0700

    Update method to parse date

--- src/MyWallet/MoneyControl.java
+++ src/MyWallet/MoneyControl.java
@@ -1,11 +1,17 @@
 package MyWallet;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MoneyControl {
+	
+	public static Date toDate(String value) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
+		return simpleDateFormat.parse("2023-11-23");
+	}
 	public static void main(String[] args){
 		Money today = new Money(80000, 0, 25000, 75000, new Date());
-		
+		Money yesterday = new Money(80000, 0, 25000, 75000, toDate("2023-11-23"));
 		System.out.println(today.toString());
 	}
 
